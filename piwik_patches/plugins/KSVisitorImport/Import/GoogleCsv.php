bf3fa48dd5988182e500e0e1623f6667b2525b1f
Kay Strobach <github@kay-strobach.de>
2014-04-07 08:45:09 +0200

    Rmove KSVisitor Import

--- piwik_patches/plugins/KSVisitorImport/Import/GoogleCsv.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Piwik_KSVisitorImport_Import_GoogleCsv extends Piwik_KSVisitorImport_Import_Abstract {
-	function import() {
-		$this->rows = 'currently unsupported';
-		return;
-	}
-}
-
-?>
