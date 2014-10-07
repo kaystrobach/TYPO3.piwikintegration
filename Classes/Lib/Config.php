95d33d7376b8cf80f6fd785acc604de31d2804d3
Kay Strobach <github@kay-strobach.de>
2014-10-07 18:02:26 +0200

    [TASK] Config fixes

--- Classes/Lib/Config.php
+++ Classes/Lib/Config.php
@@ -25,244 +25,124 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 /**
- * interact with Piwik core after download and unzip
+ * piwik_patches/plugins/TYPO3Login/TYPO3Login.php
  *
- * $Id: class.tx_piwikintegration_config.php 56783 2012-01-26 17:19:37Z kaystrobach $
+ * FE controller class
+ *
+ * $Id: TYPO3Login.php 40947 2010-12-08 07:05:46Z kaystrobach $
  *
  * @author Kay Strobach <typo3@kay-strobach.de>
  */
+namespace Piwik\Plugins\TYPO3Login;
+
+require PIWIK_INCLUDE_PATH.'/plugins/TYPO3Login/Auth.php';
 
 
-class tx_piwikintegration_config {
-	private static $configObject = null;
-	private        $installer    = null;
-	private        $initPiwikFW  = false;
-	private        $initPiwikDB  = false;
+/**
+ * Class for authentification plugin
+ * 
+ * @author  Kay Strobach <typo3@kay-strobach.de>
+ * @link http://kay-strobach.de
+ * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+ *
+ * @package Piwik_TYPO3Login
+ */
+class TYPO3Login extends \Piwik\Plugin
+{
+
 	/**
+	 * returns registered hooks
 	 *
+	 * @return	array		array of hooks
 	 */
-	private function __construct() {
-		$this->installer = tx_piwikintegration_install::getInstaller();
-		$this->initPiwikFrameWork();
+	function getListHooksRegistered()
+	{
+		$hooks = array(
+			'Request.initAuthenticationObject'	    => 'initAuthenticationObject',
+			'User.isNotAuthorized'	            => 'noAccess',
+			'API.Request.authenticate'                      => 'ApiRequestAuthenticate',
+			'Login.initSession'                             => 'initSession',
+		);
+		return $hooks;
+	}
+	function noAccess( $notification )
+	{
+		$exception  = $notification->getNotificationObject();
+		$exceptionMessage = $exception->getMessage();
+		$controller = new \Piwik\Plugins\TYPO3Login\Controller();
+		$controller->login($exceptionMessage);
 	}
 	/**
+	 * init the authentification object
 	 *
-	 */	 	
-	public static function getConfigObject() {
-		if(self::$configObject == null) {
-			self::$configObject = new tx_piwikintegration_config();
-		}
-		return self::$configObject;
-	}
-	/**
-	 *
+	 * @return	void
 	 */
-	function initPiwikFrameWork() {
-		if($this->initPiwikFW) {
-			$this->initPiwikFW = true;
+	function initAuthenticationObject()
+	{
+		$config = \Piwik\Config::getInstance();
+		
+		$auth = new \Piwik\Plugins\TYPO3Login\Auth();
+        \Piwik\Registry::set('auth', $auth);
+
+     	$action = \Piwik\Piwik::getAction();
+		if(\Piwik\Piwik::getModule() === 'API'
+			&& (empty($action) || $action == 'index'))
+		{
 			return;
 		}
 
-		//load files from piwik
-		if(!defined('PIWIK_INCLUDE_PATH')) {
-			define('PIWIK_INCLUDE_PATH', PATH_site.'typo3conf/piwik/piwik/');
-			define('PIWIK_USER_PATH'   , PATH_site.'typo3conf/piwik/piwik/');
+		$authCookieName = $config->General['login_cookie_name'];
+		$authCookieExpiry = time() + $config->General['login_cookie_expire'];
+		$authCookie = new \Piwik\Cookie($authCookieName, $authCookieExpiry);
+		$defaultLogin = 'anonymous';
+		$defaultTokenAuth = 'anonymous';
+		if($authCookie->isCookieFound())
+		{
+			$defaultLogin = $authCookie->get('login');
+			$defaultTokenAuth = $authCookie->get('token_auth');
 		}
-		if(!defined('PIWIK_INCLUDE_SEARCH_PATH')) {
-			define('PIWIK_INCLUDE_SEARCH_PATH',
-				  PIWIK_INCLUDE_PATH . '/core'
-				. PATH_SEPARATOR . PIWIK_INCLUDE_PATH . '/libs'
-				. PATH_SEPARATOR . PIWIK_INCLUDE_PATH . '/plugins'
-				. PATH_SEPARATOR . get_include_path());
-			@ini_set('include_path', PIWIK_INCLUDE_SEARCH_PATH);
-			@set_include_path(PIWIK_INCLUDE_SEARCH_PATH);
+		$auth->setLogin($defaultLogin);
+		$auth->setTokenAuth($defaultTokenAuth);
+	}
+	function initSession($notification)
+	{
+		$config = \Piwik\Config::getInstance();
+		
+		$info = $notification->getNotificationObject();
+		$login = $info['login'];
+		$md5Password = $info['md5Password'];
+		
+		$tokenAuth = \Piwik\Plugins\TYPO3Login\Auth::getTokenAuth($login, $md5Password);
+	
+		$auth = \Zend_Registry::get('auth');
+		$auth->setLogin($login);
+		$auth->setTokenAuth($tokenAuth);
+
+		$authResult = $auth->authenticate();
+
+		if(!$authResult->isValid())
+		{
+			/** @todo find translator */
+			throw new \Exception('Login_LoginPasswordNotCorrect');
 		}
+		$ns = new \Zend_Session_Namespace('Piwik_Login.referer');
+		unset($ns->referer);
 
-		set_include_path(PIWIK_INCLUDE_PATH
-					. PATH_SEPARATOR . PIWIK_INCLUDE_PATH . '/libs/'
-					. PATH_SEPARATOR . PIWIK_INCLUDE_PATH . '/plugins/'
-					. PATH_SEPARATOR . get_include_path());
+		$authCookieName = $config->General['login_cookie_name'];
+		$authCookieExpiry = time() + $config->General['login_cookie_expire'];
+		$authCookiePath = $config->General['login_cookie_path'];
+		$cookie = new \Piwik\Cookie($authCookieName, $authCookieExpiry, $authCookiePath);
+		$cookie->set('login', $login);
+		$cookie->set('token_auth', $tokenAuth);
+		$cookie->save();
 
-		include_once PIWIK_INCLUDE_PATH . 'libs/upgradephp/upgrade.php';
-		include_once PIWIK_INCLUDE_PATH . 'vendor/autoload.php';
-
-		//create config object
-		try {
-			$config = \Piwik\Config::getInstance();
-			$config->init();
-		} catch(\Exception $e) {
-
-		}
+		\Zend_Session::regenerateId();
 	}
-	function initPiwikDatabase($noLoadConfig = false) {
-		$this->initPiwikFrameWork();
-		if($this->initPiwikDB) {
-			$this->initPiwikDB = true;
-			return;
-		}
-		#include_once(PIWIK_INCLUDE_PATH . '/core/Option.php');
-		#if($noLoadConfig===true) {
-		#	Piwik::createConfigObject(PIWIK_INCLUDE_PATH.'config/config.ini.php');
-		#}
-		\Piwik\Db::createDatabaseObject();
-	}
-	function makePiwikConfigured() {
-		$this->initPiwikFrameWork();
-
-		#Piwik::setUserIsSuperUser(TRUE);
-
-		//userdata
-		$this->setOption('superuser','login'        ,md5(microtime()));
-		$this->setOption('superuser','password'     ,md5(microtime()));
-		$this->setOption('superuser','email'        ,$GLOBALS["BE_USER"]->user['email']);
-
-		//Database
-		$hostAndPort = explode(':', TYPO3_db_host);
-		if(count($hostAndPort) == 2) {
-			$this->setOption('database' ,'host'     , $hostAndPort[0]);
-			$this->setOption('database' ,'port'     , $hostAndPort[1]);
-		} else{
-			$this->setOption('database' ,'host'     ,TYPO3_db_host);
-		}
-
-		$this->setOption('database' ,'username'     ,TYPO3_db_username);
-		$this->setOption('database' ,'password'     ,TYPO3_db_password);
-		$this->setOption('database' ,'dbname'       ,TYPO3_db);
-		$this->setOption('database' ,'tables_prefix','user_piwikintegration_');
-		$this->setOption('database' ,'adapter'      ,"PDO_MYSQL");
-
-		//General
-		$this->setOption('General'  ,'show_website_selector_in_user_interface',0);
-		$this->setOption('General'  ,'serve_widget_and_data'                  ,0);
-
-		//Disable the frame detection of Piwik
-		$this->setOption('General'  ,'enable_framed_pages'                    ,1);
-		$this->setOption('General'  ,'enable_framed_logins'                   ,1);
-		$this->setOption('General'  ,'enable_framed_settings'                 ,1);
-
-		//init all plugins
-
-
-		//set Plugins
-		$this->disablePlugin('ExampleAPI');
-		$this->disablePlugin('ExamplePlugin');
-		$this->disablePlugin('ExampleRssWidget');
-		$this->disablePlugin('ExampleUI');
-		$this->disablePlugin('Login');
-		$this->enableSuggestedPlugins();
-
-		//create PiwikTables, check wether base tables already exist
-		$this->installDatabase();
+	function ApiRequestAuthenticate($notification)
+	{
+		$tokenAuth = $notification->getNotificationObject();
+		\Zend_Registry::get('auth')->setLogin($login = null);
+		\Zend_Registry::get('auth')->setTokenAuth($tokenAuth);
 	}
 
-	function enableSuggestedPlugins() {
-		$this->enablePlugin('TYPO3Login');
-		#$this->enablePlugin('TYPO3Menu');
-		#$this->enablePlugin('TYPO3Widgets');
-		#$this->enablePlugin('DBStats');
-		$this->enablePlugin('Zeitgeist');
-	}
-
-	function installDatabase() {
-		$this->initPiwikDatabase(true);
-		$tablesInstalled = \Piwik\DbHelper::getTablesInstalled();
-		if(count($tablesInstalled) == 0) {
-			\Piwik\DbHelper::createTables();
-			\Piwik\DbHelper::createAnonymousUser();
-			$updater = new \Piwik\Updater();
-			//set Piwikversion
-			$updater->recordComponentSuccessfullyUpdated('core', \Piwik\Version::VERSION);
-		}
-	}
-
-	/**
-	 * This function makes a page statistics accessable for a user
-	 * call it with $this->pageinfo['uid'] as param from a backend module
-	 *
-	 * @param    integer $uid : pid for which the user will get access
-	 * @throws Exception
-	 * @return    void
-	 */
-	function correctUserRightsForPid($uid) {
-		$this->initPiwikFrameWork();
-		if($uid <= 0 || $uid!=intval($uid)) {
-			throw new Exception('Problem with uid in tx_piwikintegration_helper.php::correctUserRightsForPid');
-		}
-		$beUserName = $GLOBALS['BE_USER']->user['username'];
-		/**
-		 * ensure, that user's right are added to the database
-		 * tx_piwikintegration_access		 
-		 */
-		if($GLOBALS['BE_USER']->user['admin']!=1) {
-			$erg = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
-					'*',
-					tx_piwikintegration_div::getTblName('access'),
-					'login="'.$beUserName.'" AND idsite='.$this->getPiwikSiteIdForPid($uid),
-					'',
-					'',
-					'0,1'
-			);
-			if(count($erg)==0) {
-				$GLOBALS['TYPO3_DB']->exec_INSERTquery(
-					tx_piwikintegration_div::getTblName('access'),
-					array(
-						'login' => $beUserName,
-						'idsite'=> $this->getPiwikSiteIdForPid($uid),
-						'access'=> 'view',
-					)
-				);
-			}
-		}
-	}
-	function getTablePrefix() {
-		#throw new Exception('config. getTablePrefix is deprecated');
-		return $this->tablePrefix = $this->getOption('database','tables_prefix');
-	}
-    function getDBName() {
-		return $this->dbName = $this->getOption('database','dbname');
-	}
-    function getT3DBName() {
-		return $this->T3DBName = $this->getOption('database','t3dbname');
-	}
-	function setOption($sectionName,$option,$value) {
-		$this->initPiwikFrameWork();
-		$piwikConfig = \Piwik\Config::getInstance();
-		$section     = $piwikConfig->$sectionName;
-		$section[$option] = $value;
-		$piwikConfig->$sectionName = $section;
-		$piwikConfig->forceSave();
-	}
-	function getOption($sectionName,$option) {
-		$this->initPiwikFrameWork();
-		$piwikConfig = \Piwik\Config::getInstance();
-		$section     = $piwikConfig->$sectionName;
-		return $section[$option];
-	}
-	function enablePlugin($plugin) {
-		$this->initPiwikFrameWork();
-		if(!\Piwik\Plugin\Manager::getInstance()->isPluginActivated($plugin)) {
-			try {
-				\Piwik\Plugin\Manager::getInstance()->activatePlugin($plugin);
-				\Piwik\Plugin\Manager::getInstance()->loadPlugins( \Piwik\Config::getInstance()->Plugins['Plugins'] );
-				#\Piwik\Plugin\Manager::getInstance()->installLoadedPlugins();
-			} catch(Exception $e) {
-
-			}
-		}
-		return;
-	}
-	function disablePlugin($plugin) {
-		$this->initPiwikFrameWork();
-		if(\Piwik\Plugin\Manager::getInstance()->isPluginActivated($plugin)) {
-			try {
-				\Piwik\Plugin\Manager::getInstance()->deactivatePlugin($plugin);
-			} catch(Exception $e) {
-
-			}
-		}
-		return;
-	}
-	function getJsForUid($uid) {
-		return '--';
-	}
 }
