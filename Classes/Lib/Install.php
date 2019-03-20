83baf95fd2afb6f366e2782dfc92e443dcc1777e
Christopher <4834755+Chrissitopher@users.noreply.github.com>
2019-03-20 12:00:45 +0100

    Update Install.php

--- Classes/Lib/Install.php
+++ Classes/Lib/Install.php
@@ -28,9 +28,9 @@ namespace KayStrobach\Piwikintegration\Lib;
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 
 /**
  * Handles the first steps of install and returns config object for next steps.
