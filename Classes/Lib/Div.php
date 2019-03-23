12071fa3bea376487175f9b3889f24dd778ee2fd
Christopher <4834755+Chrissitopher@users.noreply.github.com>
2019-03-23 07:17:45 +0100

    TemplateService->init() is removed in TYPO3 10 (#104)
    * TemplateService->init() is removed in TYPO3 10
    
    TemplateService->init() will be removed in TYPO3 v10.0, __construct() does the job.
    
    * Update Div.php
    
    * Update Div.php

--- Classes/Lib/Div.php
+++ Classes/Lib/Div.php
@@ -109,9 +109,7 @@ class Div
         }
         //parse ts template
         $template_uid = 0;
-        $tmpl = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\TypoScript\\ExtendedTemplateService');    // Defined global here!
-            $tmpl->tt_track = 0;    // Do not log time-performance information
-            $tmpl->init();
+        $tmpl = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\TypoScript\ExtendedTemplateService::class);    // Defined global here!
 
         $tplRow = $tmpl->ext_getFirstTemplate($uid, $template_uid);
         if (is_array($tplRow) || 1) {    // IF there was a template...
