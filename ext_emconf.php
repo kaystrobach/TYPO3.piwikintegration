502a5e8174b76b80bda4a9e7852bce39cba7bb64
Christopher <4834755+Chrissitopher@users.noreply.github.com>
2019-12-28 19:04:58 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company'   => '',
     'constraints'      => [
         'depends' => [
-            'php'   => '7.2.0-7.3.99',
+            'php'   => '7.2.0-7.4.99',
             'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [
