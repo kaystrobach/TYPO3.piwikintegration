13855b58a0db9d54c2c64b54f68da9cb513cb515
Christopher <4834755+Chrissitopher@users.noreply.github.com>
2021-07-29 17:16:53 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "piwikintegration".
- *
- * Auto generated 11-03-2013 20:23
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title'            => 'Matomo Backend integration',
     'description'      => 'Uses EXT:piwik to inserts Data in the HTML header and gives BE-Users the right to see the data for their sites. Autoupdate of Matomo will work as TYPO3-Admin!',
@@ -24,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints'      => [
         'depends' => [
             'php'   => '7.2.0-8.0.99',
-            'typo3' => '10.0.0-10.4.99',
+            'typo3' => '10.0.0-11.5.99',
         ],
         'conflicts' => [
             'dbal' => '1.0.0-99.0.0',
